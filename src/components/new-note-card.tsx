3e06bcb4d73e23d191998abd4deb0391fb9ff778
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-02-09 19:00:09 -0300

    NLW Expert | Aula 03

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,17 +1,26 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { X } from "@phosphor-icons/react";
 import { ChangeEvent, FormEvent, useState } from "react";
-import { toast } from 'sonner';
+import { toast } from "sonner";
 
-export function NewNoteCard() {
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void;
+}
+
+
+let SpeechRecognition: SpeechRecognition | null = null;
+
+export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
-  const [content, setContent] = useState('')
+  const [isRecording, setIsRecording] = useState(false);
+  const [content, setContent] = useState("");
+
   function handleStartEditor() {
     setShouldShowOnboarding(false);
   }
 
   function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>) {
-    setContent(event.target.value)
+    setContent(event.target.value);
 
     if (event.target.value === "") {
       setShouldShowOnboarding(true);
@@ -19,9 +28,68 @@ export function NewNoteCard() {
   }
 
   function handleSaveNote(event: FormEvent) {
-    event.preventDefault()
+    event.preventDefault();
 
-    toast.success('Nota criada com sucesso')
+    if (content === "") {
+      return;
+    }
+
+    onNoteCreated(content);
+
+    setContent("");
+    setShouldShowOnboarding(true);
+
+    toast.success("Nota criada com sucesso");
+  }
+
+  function handleStartRecording() {
+    const isSpeechRecognitionAPIavailable =
+      "SpeechRecognition" in window || "webkitSpeechRecognition" in window;
+
+    if (!isSpeechRecognitionAPIavailable) {
+      alert("Infelizmente seu navegador não suporta a API de gravação");
+      return;
+    }
+
+    setIsRecording(true);
+    setShouldShowOnboarding(false);
+
+    const SpeechRecognitionAPI =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    SpeechRecognition = new SpeechRecognitionAPI();
+
+    SpeechRecognition.lang = "pt-BR";
+    SpeechRecognition.continuous = true;
+    SpeechRecognition.maxAlternatives = 1;
+    SpeechRecognition.interimResults = true;
+
+    SpeechRecognition.onresult = (event) => {
+      const transcription = Array.from(event.results).reduce((text, result) => {
+        return text.concat(result[0].transcript);
+      }, "");
+
+      setContent(transcription);
+    };
+
+    SpeechRecognition.onerror = (event) => {
+      console.error(event);
+      if (event.error === "network") {
+        alert(
+          "A network error occurred while trying to use the SpeechRecognition API. Please check your internet connection and try again."
+        );
+      }
+    };
+
+    SpeechRecognition.start();
+  }
+
+  function handleStopRecording() {
+    setIsRecording(false);
+
+    if (SpeechRecognition !== null) {
+      SpeechRecognition.stop();
+    }
   }
 
   return (
@@ -37,12 +105,12 @@ export function NewNoteCard() {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50 ">
-          <Dialog.Content className="fixed left-1/2 top-1/2 overflow-hidden -translate-x-1/2 -translate-y-1/2  max-w-[640px] h-[60vh] w-full bg-slate-700 rounded-md flex flex-col outline-none ">
-            <Dialog.Close className=" absolute right-0 p-1.5 text-slate-400 top-0 bg-slate-800 hover:text-slate-100">
+          <Dialog.Content className="fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 overflow-hidden md:-translate-x-1/2 md:-translate-y-1/2  md:max-w-[640px] md:h-[60vh] w-full bg-slate-700 md:rounded-md flex flex-col outline-none ">
+            <Dialog.Close className=" absolute right-0 p-1.5 text-slate-400 top-0  hover:text-slate-100">
               <X size={20} />
             </Dialog.Close>
 
-            <form onSubmit={handleSaveNote} className="flex-1 flex flex-col">
+            <form className="flex-1 flex flex-col">
               <div className="flex flex-1 flex-col gap-3 p-5">
                 <span className="text-sm font-medium text-slate-300">
                   Adicionar nota
@@ -50,12 +118,16 @@ export function NewNoteCard() {
                 {shouldShowOnboarding ? (
                   <p className="text-sm leading-6 text-slate-400 overflow-hidden">
                     Comece{" "}
-                    <button className="font-md text-lime-400 hover:underline">
-                      {" "}
+                    <button
+                      type="button"
+                      onClick={handleStartRecording}
+                      className="font-md text-lime-400 hover:underline"
+                    >
                       gravando uma nota{" "}
                     </button>{" "}
                     em áudio ou se preferir{" "}
                     <button
+                      type="button"
                       onClick={handleStartEditor}
                       className="font-md text-lime-400 hover:underline"
                     >
@@ -69,16 +141,30 @@ export function NewNoteCard() {
                     autoFocus
                     className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
                     onChange={handleContentChanged}
+                    value={content}
                   ></textarea>
                 )}
               </div>
 
-              <button
-                type="submit"
-                className="w-full bg-lime-400 text-lime-950 py-4 text-sm text-slate-300 outline-none font-medium hover:bg-lime-300"
-              >
-                Salvar nota
-              </button>
+              {isRecording ? (
+                <button
+                  type="button"
+                  onClick={handleStopRecording}
+                  className="w-full flex items-center justify-center gap-2 bg-slate-900 text-slate-300 py-4 text-sm  outline-none font-medium hover:bg-slate-800 "
+                >
+                  <div className="size-3 rounded-full bg-red-500 animate-pulse" />
+                  <span className="text-red-600">Gravando!</span> (clique p/
+                  interromper)
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  className="w-full bg-lime-400 text-lime-950 py-4 text-sm  outline-none font-medium hover:bg-lime-300"
+                  onClick={handleSaveNote}
+                >
+                  Salvar nota
+                </button>
+              )}
             </form>
           </Dialog.Content>
         </Dialog.Overlay>
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,18 +1,25 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
-import { X } from '@phosphor-icons/react' ;
+import { X } from "@phosphor-icons/react";
 
 interface NoteCardProps {
-  note: { date: Date; content: string };
+  note: { 
+    id: string
+    date: Date; 
+    content: string };
+  onNoteDeleted: (id: string) => void;
 }
 
-export function NoteCard({ note }: NoteCardProps) {
+export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
   return (
     <Dialog.Root>
       <Dialog.Trigger className="bg-slate-800 text-left rounded-md p-5 flex flex-col gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
         <span className="text-sm font-medium text-slate-300">
-          {note.date.toISOString()}
+          {formatDistanceToNow(note.date, {
+            locale: ptBR,
+            addSuffix: true,
+          })}
         </span>
 
         <p className="text-sm leading-6 text-slate-400 overflow-hidden">
@@ -24,11 +31,11 @@ export function NoteCard({ note }: NoteCardProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50 ">
-          <Dialog.Content className="fixed left-1/2 top-1/2 overflow-hidden -translate-x-1/2 -translate-y-1/2  max-w-[640px] h-[60vh] w-full bg-slate-700 rounded-md flex flex-col outline-none ">
+          <Dialog.Content className="fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 overflow-hidden md:-translate-x-1/2 md:-translate-y-1/2  md:max-w-[640px] md:h-[60vh] w-full bg-slate-700 md:rounded-md flex flex-col outline-none ">
             <Dialog.Close className=" absolute right-0 p-1.5 text-slate-400 top-0 bg-slate-800 hover:text-slate-100">
-            <X size={20}  />
+              <X size={20} />
             </Dialog.Close>
-            
+
             <div className="flex flex-1 flex-col gap-3 p-5">
               <span className="text-sm font-medium text-slate-300">
                 {formatDistanceToNow(note.date, {
@@ -45,8 +52,13 @@ export function NoteCard({ note }: NoteCardProps) {
             <button
               type="button"
               className="w-full bg-slate-800 py-4 text-sm text-slate-300 outline-none font-medium group"
+              onClick={() => onNoteDeleted(note.id)}
             >
-              Deseja <span className="text-red-400 group-hover:underline"> apagar essa nota?</span>
+              Deseja{" "}
+              <span className="text-red-500 group-hover:underline">
+                {" "}
+                apagar essa nota?
+              </span>
             </button>
           </Dialog.Content>
         </Dialog.Overlay>
